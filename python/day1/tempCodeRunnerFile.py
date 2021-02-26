75fc07a56861659a52aeaf98eafd4048753f9702
pip-pipo <codemi692@gmail>
2021-02-25 21:49:59 -1100

    :rocket:more code practice have dit

---
+++ python/day1/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print('\033[31m' + 'some red text')
+# print('\033[39m') # and reset to default color
+# print(Back.BLUE + "morsalin" )
--- python/day1/terminal_color.py
+++ python/day1/terminal_color.py
@@ -4,6 +4,15 @@ from colorama import Fore ,Back ,Style
 
 colorama.init()
 
-print('\003[31m' + 'morsalin')
+# print(Fore.RED + 'some red text')
+# print(Back.CYAN + 'and with a green background')
+# print(Style.DIM + 'and in dim text')
+# print(Style.RESET_ALL)
+# print('back to normal now')
+# print('\033[31m' + 'some red text')
+# print('\033[39m') # and reset to default color
+# print(Back.BLUE + "morsalin" )
 
-print('\033[39m')
+# print(colored('Hello, World!', 'green', 'on_red'))
+print(Fore.BLACK)
+print(Back.MAGENTA +"morsalin")
