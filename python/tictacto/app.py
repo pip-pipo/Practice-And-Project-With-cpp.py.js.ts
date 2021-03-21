202439ec40d10158f29cda43dd156f9008322e56
pip-pipo <codemi692@gmail>
2021-03-20 15:35:08 -1100

    :tada:python/day2/my.txt added

--- python/tictacto/app.py
+++ python/tictacto/app.py
@@ -52,11 +52,51 @@
 # main()
 
 
-import pyfiglet
+# import pyfiglet
 
 
-loadTxtShow = pyfiglet.figlet_format(' PYDB',font="big")
+# loadTxtShow = pyfiglet.figlet_format(' PYDB',font="big")
 
-print(loadTxtShow
+# print(loadTxtShow
 
-)
+# )
+
+
+import numpy as np
+
+isOpen = True
+
+while(isOpen):
+    print("=============PyJonathon==========")
+    print("press 1 for getting the menu")
+    print("press 2 for exit")
+    whatPressed = int(input())
+    print("=============End Of PyJonathon==========")
+    if(whatPressed == 1):
+            print("+++++++++Option's+++++++++++")
+            print("press 1 for making zeros")
+            print("press 2 for making random array")
+            print("press 3 for making ndarray")
+            print("press 4 for go back")
+            optionSelect = int(input())
+            print("+++++++++End Of Option's+++++++++++")
+            if(optionSelect==1):
+                print("%%%%%%%% Making Zeros %%%%%%")
+                print("How Much zeros do you want?")
+                howMuchZerosWant = int(input())
+                print(np.zeros((howMuchZerosWant,howMuchZerosWant)))
+                print("%%%%%%%% end of Making Zeros %%%%%%")
+
+            if(optionSelect==2):
+                pass
+            if(optionSelect==3):
+                pass
+            if(optionSelect==4):
+                whatPressed=455
+
+    elif(whatPressed == 2):
+            isOpen = False
+    else:
+            print("--------------warning!---------------")
+            print("\t please press the currect option's\n")
+            print("============================================")
